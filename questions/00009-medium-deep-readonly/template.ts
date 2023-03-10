1e0d49957a16ffbb24299f313b3785cdc2e8e178
ban117 <bcam117@gmail.com>
2023-03-10 17:08:20 +0100

    deepreadonly

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,4 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> =
+  keyof T extends never
+    ? T // T isn't enumerable, so it's a primative
+    : { readonly [x in keyof T]: DeepReadonly<T[x]> }
--- questions/00009-medium-deep-readonly/test-cases.ts
+++ questions/00009-medium-deep-readonly/test-cases.ts
@@ -2,7 +2,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<DeepReadonly<X1>, Expected1>>,
-  Expect<Equal<DeepReadonly<X2>, Expected2>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>, // todo
 ]
 
 type X1 = {
@@ -54,3 +54,16 @@ type Expected1 = {
 }
 
 type Expected2 = { readonly a: string } | { readonly b: number }
+
+// *** Tests ***
+type T1 = DeepReadonly<X1>
+//    ^?
+
+type T2 = DeepReadonly<X2>
+//    ^?
+
+// *** Solution ***
+type DeepReadonly<T> =
+  keyof T extends never
+    ? T // T isn't enumerable, so it's a primative
+    : { readonly [x in keyof T]: DeepReadonly<T[x]> }
